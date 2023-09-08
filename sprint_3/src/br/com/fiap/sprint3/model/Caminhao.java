09547be9b60a22d70f01a0153206569466bbef7a
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-08 18:25:09 -0300

    Adição de model

---
+++ sprint_3/src/br/com/fiap/sprint3/model/Caminhao.java
@@ -0,0 +1,51 @@
+package br.com.fiap.sprint3.model;
+
+public class Caminhao extends Veiculo{
+	//Atributos
+	private int numeroDeEixos;
+	private double pesoTotalEmToneladas;
+	private String idVinculo;
+	
+	//Construtor
+	public Caminhao(String modelo, String chassi, String renavam, String cor, int ano, double altura,
+			double comprimento, double largura, int numeroDeEixos, double pesoTotalEmToneladas, String idVinculo) {
+		super(modelo, chassi, renavam, cor, ano, altura, comprimento, largura);
+		this.numeroDeEixos = numeroDeEixos;
+		this.pesoTotalEmToneladas = pesoTotalEmToneladas;
+		this.idVinculo = idVinculo;
+	}//Caminhao
+
+	//Getters
+	public int getNumeroDeEixos() {
+		return numeroDeEixos;
+	}
+
+	public double getPesoTotalEmToneladas() {
+		return pesoTotalEmToneladas;
+	}
+
+	public String getIdVinculo() {
+		return idVinculo;
+	}
+
+	//Setters
+	public void setNumeroDeEixos(int numeroDeEixos) {
+		this.numeroDeEixos = numeroDeEixos;
+	}
+
+	public void setPesoTotalEmToneladas(double pesoTotalEmToneladas) {
+		this.pesoTotalEmToneladas = pesoTotalEmToneladas;
+	}
+
+	public void setIdVinculo(String idVinculo) {
+		this.idVinculo = idVinculo;
+	}
+	
+	//Métodos
+	@Override
+	public String retornarInformacoes() {
+		return super.retornarInformacoes() + ", Numero de Eixos: " + numeroDeEixos
+				+ ", Peso Total: " + pesoTotalEmToneladas;
+	}
+	
+}//CLASS
