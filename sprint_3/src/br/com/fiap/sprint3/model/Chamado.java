eb8e29f5e642f47792a9ca5f51c2655ec9a45659
gustavocarvalho1997 <gustavocarvalho1997@hotmail.com>
2023-09-08 23:11:37 -0300

    Criação model chamado

---
+++ sprint_3/src/br/com/fiap/sprint3/model/Chamado.java
@@ -0,0 +1,10 @@
+package br.com.fiap.sprint3.model;
+
+public class Chamado {
+	//Atributos
+	private Endereco endereco;
+	private Veiculo guinchoEscolhido;
+	private boolean chamadoAtivo, chamadoPausado, chamadoFechado;
+	
+	
+}//CLASS
